c7ea003462079f0b88252cdc0f2404248a0d63db
emmajane <emmajane@naver.com>
2023-09-11 14:46:30 +0900

    0911 JM chatting page 상단바

--- frontend/a705/lib/chatting_page.dart
+++ frontend/a705/lib/chatting_page.dart
@@ -11,9 +11,38 @@ class _ChattingPageState extends State<ChattingPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Container(
-            color: Colors.lightGreen
-        )
-    );
+        child: Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 80,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)),
+            color: Color(0xFFFFD954),
+          ),
+          child: const Center(
+            child: Text(
+              "채팅",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Container(
+            margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            width: double.infinity,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(color: Colors.black38),
+            ))
+      ],
+    ));
   }
 }
