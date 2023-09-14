7155541847708659b035aeeceef1b1f62dbc5f0d
emmajane <emmajane@naver.com>
2023-09-14 17:50:29 +0900

    0914 JM choose location page 수정

--- frontend/a705/lib/choose_location_page.dart
+++ frontend/a705/lib/choose_location_page.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -24,6 +26,23 @@ class _ChooseLocationPageState extends State<ChooseLocationPage> {
     return position;
   }
 
+  Future<LatLng> getCenter() async {
+    final GoogleMapController controller = await _controller.future;
+    LatLngBounds visibleRegion = await controller.getVisibleRegion();
+    LatLng centerLatLng = LatLng(
+      (visibleRegion.northeast.latitude + visibleRegion.southwest.latitude) / 2,
+      (visibleRegion.northeast.longitude + visibleRegion.southwest.longitude) /
+          2,
+    );
+
+    return centerLatLng;
+  }
+
+  double latitude = 37.5013068;
+  double longitude = 127.0396597;
+
+  String addr = "";
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,21 +50,70 @@ class _ChooseLocationPageState extends State<ChooseLocationPage> {
         backgroundColor: Colors.white,
         appBar: AppBar(
           centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0,
           title: const Text(
             '장소 선택',
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new_rounded,
+              color: Colors.black,
+            ),
           ),
         ),
         body: Stack(children: [
+          Column(
+            children: [
+              const SizedBox(height: 40),
+              const Text(
+                '거래하실 장소를 선택해 주세요.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              const Center(child: Text('선택하신 장소')),
+              Container(
+                margin: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.black87),
+                ),
+                child: Center(
+                  child: Text(
+                    addr,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
           SizedBox(
             height: double.infinity,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                // Container(height: 150, color: Colors.red),
                 SizedBox(
-                  height: 500,
+                  height: MediaQuery.of(context).size.height / 3 * 2,
                   child: GoogleMap(
+                    onCameraMove: (object) => {
+                      setState(() {
+                        currentPosition = object.target;
+                        latitude = object.target.latitude;
+                        longitude = object.target.longitude;
+                      })
+                    },
                     mapType: MapType.normal,
                     onMapCreated: (GoogleMapController controller) {
                       _controller.complete(controller);
@@ -67,7 +135,7 @@ class _ChooseLocationPageState extends State<ChooseLocationPage> {
           ),
           Positioned(
             // alignment: Alignment.center,
-            bottom: 250,
+            bottom: MediaQuery.of(context).size.height / 3 * 1,
             left: MediaQuery.of(context).size.width / 2 - 25,
             child: SizedBox(
               width: 50,
@@ -75,7 +143,7 @@ class _ChooseLocationPageState extends State<ChooseLocationPage> {
             ),
           ),
           Positioned(
-            bottom: 30,
+            bottom: 100,
             right: 30,
             child: FloatingActionButton(
               onPressed: () async {
@@ -89,7 +157,53 @@ class _ChooseLocationPageState extends State<ChooseLocationPage> {
                 color: Colors.black87,
               ),
             ),
-          )
+          ),
+          Positioned(
+            bottom: 30,
+            child: GestureDetector(
+              onTap: () async {
+                LatLng _currentCenter = await getCenter();
+                List<Placemark> placemark = await placemarkFromCoordinates(
+                    _currentCenter.latitude, _currentCenter.longitude);
+
+                // print('country: ${placemark.reversed.last.country}');
+                // print('locality: ${placemark.reversed.last.locality}');
+                // print('street: ${placemark.reversed.last.street}');
+                // print('subLocality: ${placemark.reversed.last.subLocality}');
+                // print('name: ${placemark.reversed.last.name}');
+                // print(
+                //     'isoCountryCode: ${placemark.reversed.last.isoCountryCode}');
+                // print('postalCode: ${placemark.reversed.last.postalCode}');
+                // print(
+                //     'administrativeArea: ${placemark.reversed.last.administrativeArea}');
+                // print(
+                //     'subAdministrativeArea: ${placemark.reversed.last.subAdministrativeArea}');
+                // print(
+                //     'subThoroughfare: ${placemark.reversed.last.subThoroughfare}');
+                // print('thoroughfare: ${placemark.reversed.last.thoroughfare}');
+                // print('hashCode: ${placemark.reversed.last.hashCode}');
+
+                setState(() {
+                  addr = "${placemark.reversed.last.subLocality} ${placemark.reversed.last.thoroughfare}";
+                });
+              },
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                height: 50,
+                width: MediaQuery.of(context).size.width - 60,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFD954),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text(
+                    '선택 완료',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ]),
       ),
     );
