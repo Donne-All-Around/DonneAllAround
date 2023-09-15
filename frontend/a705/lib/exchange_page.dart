60f68562bc1e5b44b2e78e946c35d7de1b35a2b1
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-09-15 17:07:07 +0900

    0915 ha 환율검색 페이지 구조 진행 중

--- frontend/a705/lib/exchange_page.dart
+++ frontend/a705/lib/exchange_page.dart
@@ -10,55 +10,117 @@ class ExchangePage extends StatefulWidget {
 class _ExchangePageState extends State<ExchangePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 80,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-              color: Color(0xFFFFD954),
-            ),
-            child:  Center(
-              child: RichText(
-                text: const TextSpan(
-                  children:  [
-                    TextSpan(
-                      text: "환율",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+                children: [
+                  // 상단 바 (환율 검색)
+                  Container(
+                    width: double.infinity,
+                    height: 80,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                      color: Color(0xFFFFD954),
+                    ),
+                    child:  Center(
+                      child: RichText(
+                        text: const TextSpan(
+                          children:  [
+                            TextSpan(
+                              text: "환율",
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                              ),
+                            ),
+                            TextSpan(
+                              text: " 검색",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        // textAlign: TextAlign.center,
+                        ),
                       ),
                     ),
-                    TextSpan(
-                      text: "검색",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
+                  ),
+                  const SizedBox(height: 10,),
+                  // body 페이지
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    width: double.infinity,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black38),
                     ),
-                  ],
-                // textAlign: TextAlign.center,
-                ),
+                    child:Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(10, 20,40, 10),
+                              width: 230,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(color: Colors.black38),
+                              ),
+                            ),
+                            const SizedBox(width: 10,),
+                            IconButton(
+                                onPressed: (){},
+                                icon: const Icon(Icons.cached_rounded),
+                                iconSize: 40,
+                                color: Colors.grey,),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                              width: 150,
+                              height: 20,
+                              // color: Colors.red,
+                              child: Row(
+                                children: [
+                                  ElevatedButton(
+                                      onPressed: (){},
+                                    style: ElevatedButton.styleFrom(
+                                      elevation: 0,
+
+                                    ),
+                                      child: const Text('직접'),),
+                                  const SizedBox(width: 5),
+                                  ElevatedButton(
+                                      onPressed:(){},
+                                      style: ElevatedButton.styleFrom(
+                                        elevation: 0,
+                                      ),
+                                      child: const Text('이중') ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ),
           ),
-          const SizedBox(height: 10,),
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-            width: double.infinity,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.black38),
-            ),),
-        ],
+        ),
       ),
     );
   }
