f624af82ea63fa23f45fb1608e550b9d5aaee8f2
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-09-18 16:54:18 +0900

    0918 ha 환율페이지 시간추가 및 구조

--- frontend/a705/lib/exchange_page.dart
+++ frontend/a705/lib/exchange_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
+import 'package:intl/intl.dart';
 
 class ExchangePage extends StatefulWidget {
   const ExchangePage({super.key});
@@ -8,6 +10,14 @@ class ExchangePage extends StatefulWidget {
 }
 
 class _ExchangePageState extends State<ExchangePage> {
+
+  String getToday() {
+    DateTime now = DateTime.now();
+    DateFormat formatter = DateFormat('yyyy.MM.dd HH:mm');
+    var strToday = formatter.format(now);
+    return strToday;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -59,7 +69,7 @@ class _ExchangePageState extends State<ExchangePage> {
                   Container(
                     margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                     width: double.infinity,
-                    height: 300,
+                    height: 310,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
                       border: Border.all(color: Colors.black38),
@@ -70,12 +80,16 @@ class _ExchangePageState extends State<ExchangePage> {
                           children: [
                             Container(
                               margin: const EdgeInsets.fromLTRB(10, 20,40, 10),
+                              padding: const EdgeInsets.fromLTRB(15, 10, 10, 5),
                               width: 230,
                               height: 40,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(15),
                                 border: Border.all(color: Colors.black38),
                               ),
+                              child: Text(
+                                getToday(),
+                                style: const TextStyle(fontSize: 25),),
                             ),
                             const SizedBox(width: 10,),
                             IconButton(
@@ -88,7 +102,7 @@ class _ExchangePageState extends State<ExchangePage> {
                         Row(
                           children: [
                             Container(
-                              margin: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                              margin: const EdgeInsets.fromLTRB(20, 0, 0, 10),
                               width: 150,
                               height: 20,
                               // color: Colors.red,
@@ -98,14 +112,16 @@ class _ExchangePageState extends State<ExchangePage> {
                                       onPressed: (){},
                                     style: ElevatedButton.styleFrom(
                                       elevation: 0,
-
+                                        backgroundColor: Colors.green
                                     ),
-                                      child: const Text('직접'),),
+
+                                      child: const Text('직접',),),
                                   const SizedBox(width: 5),
                                   ElevatedButton(
                                       onPressed:(){},
                                       style: ElevatedButton.styleFrom(
                                         elevation: 0,
+                                        backgroundColor: Colors.red,
                                       ),
                                       child: const Text('이중') ),
                                 ],
@@ -113,15 +129,263 @@ class _ExchangePageState extends State<ExchangePage> {
                             ),
                           ],
                         ),
+                          Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                width: 340,
+                                height: 60,
+                                // color: Colors.red,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(color: Colors.black38),
+                                ),
+                              ),
+                            ],
+                          ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                              width: 340,
+                              height: 60,
+                              // color: Colors.red,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(color: Colors.black38),
+                              ),
+                            ),
+                          ],
+                        ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(270, 10, 10, 0),
+                            width: 70,
+                            height: 50,
+                            // color: const Color(0xFFFFD954),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              // border: Border.all(color: Colors.black38),
+                              color:  const Color(0xFFFFD954),
+                            ),
+                            child:IconButton(
+                              padding: const EdgeInsets.fromLTRB(5, 0, 5, 10),
+                              onPressed: (){},
+                              icon: const Icon(Icons.drag_handle_rounded),
+                              iconSize: 50,
+                              color: Colors.white,
 
+                            ),
+                          ),
+                        ],
+                      ),
                       ],
                     ),
                   ),
-                ],
+                  // 말풍선
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+                        width: 350,
+                        height: 60,
+                        child: getSenderView(
+                            ChatBubbleClipper6(type: BubbleType.sendBubble), context),
+                      ),
+                    ],
+                  ),
+                 //국가별 실시간 환율
+                     GestureDetector(
+                       // 상세페이지 이동
+                       onTap: (){} ,
+                       child: Row(
+                         children: [
+                           Container(
+                             margin: const EdgeInsets.fromLTRB(25, 10, 20, 0),
+                             width: 360,
+                             height: 50,
+                             decoration: BoxDecoration(
+                               borderRadius: BorderRadius.circular(20),
+                               border: Border.all(color: Colors.black38),
+                             ),
+                             child:
+                             const Row(
+                               children: [
+                                 SizedBox(width: 30,),
+                                  CircleAvatar(
+                                   backgroundImage: AssetImage('assets/images/australia.png',),
+                                   radius: 17,
+                                 ),
+                                 Text('  호주 (달러)' , style: TextStyle(fontSize: 17),),
+                                 Text(' AUD',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                                 SizedBox(width: 60,),
+                                 Text('853.73원',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                               ],
+                             ),
+                           ),
+                         ],
+                       ),
+                     ),
+                  GestureDetector(
+                    // 상세페이지 이동
+                    onTap: (){} ,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(25, 10, 20, 0),
+                          width: 360,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black38),
+                          ),
+                          child:
+                          const Row(
+                            children: [
+                              SizedBox(width: 30,),
+                              CircleAvatar(
+                                backgroundImage: AssetImage('assets/images/australia.png',),
+                                radius: 17,
+                              ),
+                              Text('  호주 (달러)' , style: TextStyle(fontSize: 17),),
+                              Text(' AUD',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                              SizedBox(width: 60,),
+                              Text('853.73원',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    // 상세페이지 이동
+                    onTap: (){} ,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(25, 10, 20, 0),
+                          width: 360,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black38),
+                          ),
+                          child:
+                          const Row(
+                            children: [
+                              SizedBox(width: 30,),
+                              CircleAvatar(
+                                backgroundImage: AssetImage('assets/images/australia.png',),
+                                radius: 17,
+                              ),
+                              Text('  호주 (달러)' , style: TextStyle(fontSize: 17),),
+                              Text(' AUD',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                              SizedBox(width: 60,),
+                              Text('853.73원',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    // 상세페이지 이동
+                    onTap: (){} ,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(25, 10, 20, 0),
+                          width: 360,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black38),
+                          ),
+                          child:
+                          const Row(
+                            children: [
+                              SizedBox(width: 30,),
+                              CircleAvatar(
+                                backgroundImage: AssetImage('assets/images/australia.png',),
+                                radius: 17,
+                              ),
+                              Text('  호주 (달러)' , style: TextStyle(fontSize: 17),),
+                              Text(' AUD',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                              SizedBox(width: 60,),
+                              Text('853.73원',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    // 상세페이지 이동
+                    onTap: (){} ,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(25, 10, 20, 0),
+                          width: 360,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black38),
+                          ),
+                          child:
+                          const Row(
+                            children: [
+                              SizedBox(width: 30,),
+                              CircleAvatar(
+                                backgroundImage: AssetImage('assets/images/australia.png',),
+                                radius: 17,
+                              ),
+                              Text('  호주 (달러)' , style: TextStyle(fontSize: 17),),
+                              Text(' AUD',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                              SizedBox(width: 60,),
+                              Text('853.73원',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  ],
               ),
           ),
         ),
       ),
     );
   }
+
+   // 말풍선
+  getSenderView(CustomClipper clipper, BuildContext context) => ChatBubble(
+    clipper: clipper,
+    elevation: 0,
+    alignment: Alignment.topLeft,
+    margin: const EdgeInsets.all(0),
+    backGroundColor:const Color(0xFFFFD954),
+    child: Container(
+      // constraints: const BoxConstraints(
+      //   // maxWidth: MediaQuery.of(context).size.width * 1,
+      // ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: const Row(
+        children: [
+          Text(
+            " 국가별 실시간 환율",
+            style: TextStyle(color: Colors.white, fontSize: 17),
+          ),
+          Text(
+            "을 확인 할 수 있어요! ",
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+        ],
+      ),
+    ),
+  );
 }
