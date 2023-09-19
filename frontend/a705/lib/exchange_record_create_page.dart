ef62da7724ffbf37a0da4a33505847468c10f179
yohwan <moonyh9600@gmail.com>
2023-09-19 17:46:17 +0900

    0919 YH 환전기록작성페이지드롭다운빼고완성

--- frontend/a705/lib/exchange_record_create_page.dart
+++ frontend/a705/lib/exchange_record_create_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 
 class ExchangeRecordCreatePage extends StatefulWidget {
   const ExchangeRecordCreatePage({super.key});
@@ -8,9 +10,32 @@ class ExchangeRecordCreatePage extends StatefulWidget {
 }
 
 class ExchangeRecordCreatePageState extends State<ExchangeRecordCreatePage> {
+  final _valueList = ['첫 번째', '두 번째', '세 번째'];
+  var _selectedValue = '첫 번째';
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      child: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
         child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.white,
@@ -25,15 +50,325 @@ class ExchangeRecordCreatePageState extends State<ExchangeRecordCreatePage> {
             ),
             elevation: 0,
             title: const Text(
-                '나의 환전',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                )
+              '나의 환전',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )
             ),
             centerTitle: true,
           ),
+          body: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '환전 일시',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    )
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    height: 60,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      color: Color(0xFFF2F2F2),
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        _selectDate(context);
+                      },
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.right,
+                        )
+                      )
+                    )
+                  ),
+
+                  const SizedBox(height: 15),
+                  const Text(
+                    '화폐 종류',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    height: 60,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                      color: Color(0xFFF2F2F2),
+                    ),
+                    child: Row(
+                      children: [
+                        const SizedBox(width: 10),
+                        DropdownButton(
+                          value: _selectedValue,
+                          items: _valueList.map((value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Row(
+                                children: [
+                                  const CircleAvatar(
+                                    backgroundImage: AssetImage(
+                                        'assets/images/australia.png'),
+                                    radius: 10,
+                                  ),
+                                 const SizedBox(width: 5),
+                                  Text(value),
+                                ],
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedValue = value!;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                        borderSide:
+                        BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                        borderSide:
+                        BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      suffixText: ' \$',
+                    ),
+                    cursorColor: Colors.black87,
+                    textAlign: TextAlign.end,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    '가격',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    height: 60,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                      color: Color(0xFFF2F2F2),
+                    ),
+                    child: const Row(
+                      children: [
+                        SizedBox(width: 10),
+                        CircleAvatar(
+                          backgroundImage:
+                          AssetImage('assets/images/korea.png'),
+                          radius: 10,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          '대한민국(원화) KRW',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                        borderSide:
+                        BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                        borderSide:
+                        BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      suffixText: ' ₩',
+                    ),
+                    cursorColor: Colors.black87,
+                    textAlign: TextAlign.end,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    '은행',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    )
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      color: Color(0xFFF2F2F2),
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide:
+                        BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide:
+                        BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      suffixText: ' %',
+                    ),
+                    cursorColor: Colors.black87,
+                    textAlign: TextAlign.end,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    '우대율',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    )
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      color: Color(0xFFF2F2F2),
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide:
+                          BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide:
+                          BorderSide(color: Color(0xFFF2F2F2), width: 3),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      suffixText: ' %',
+                    ),
+                    cursorColor: Colors.black87,
+                    textAlign: TextAlign.end,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      color: Color(0xFFFFD954),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '작성 완료',
+                        style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)
+                      )
+                    )
+                  )
+                ]
+              )
+            )
+          )
         )
+      )
     );
   }
 }
