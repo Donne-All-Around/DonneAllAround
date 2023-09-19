ea0360b81b89b84051da15e0789a00950d7d2f77
emmajane <emmajane@naver.com>
2023-09-19 16:37:10 +0900

    0919 JM transaction detail page 수정

--- frontend/a705/lib/transaction_detail_page.dart
+++ frontend/a705/lib/transaction_detail_page.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:a705/chatting_detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,7 +14,6 @@ class TransactionDetailPage extends StatefulWidget {
 }
 
 class _TransactionDetailPageState extends State<TransactionDetailPage> {
-
   @override
   void initState() {
     super.initState();
@@ -23,26 +23,25 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
   final List<Marker> _marker = <Marker>[];
   String image = 'assets/images/marker.png';
   final LatLng latlng = const LatLng(37.5013068, 127.0396597);
-  
+
   Future<Uint8List> getImages(String path, int width) async {
     ByteData data = await rootBundle.load(path);
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetHeight: width);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetHeight: width);
     ui.FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
   }
 
   loadData() async {
     final Uint8List markIcon = await getImages(image, 100);
-    _marker.add(
-      Marker(
-        markerId: const MarkerId('1'),
-        icon: BitmapDescriptor.fromBytes(markIcon),
-        position: latlng,
-        infoWindow: const InfoWindow(
-          title: 'Location'
-        ),
-      )
-    );
+    _marker.add(Marker(
+      markerId: const MarkerId('1'),
+      icon: BitmapDescriptor.fromBytes(markIcon),
+      position: latlng,
+      infoWindow: const InfoWindow(title: 'Location'),
+    ));
   }
 
   @override
@@ -63,106 +62,193 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
             },
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(
-                  child: Image(
-                image: AssetImage('assets/images/ausdollar.jpg'),
-              )),
-              Container(height: 1, color: Colors.black26),
-              Container(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage:
-                              AssetImage('assets/images/profile.jpg'),
-                          radius: 35,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          '옹골찬',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.yellow,
-                    )
-                  ],
-                ),
-              ),
-              Container(height: 1, color: Colors.black26),
-              Container(
+        body: Stack(children: [
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                    child: Image(
+                  image: AssetImage('assets/images/ausdollar.jpg'),
+                )),
+                Container(height: 1, color: Colors.black26),
+                Container(
                   padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  child: Column(
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Text(
-                            '호주 달러 50달러 팔아요',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
+                          CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assets/images/profile.jpg'),
+                            radius: 30,
                           ),
                           SizedBox(width: 10),
-                          Text('1시간 전'),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        '7월에 호주 여행 다녀와서 남은 돈이에요 역삼역 근처에서 직거래 희망합니다',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      const SizedBox(height: 10),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.location_on,
-                                color: Colors.red,
-                              ),
-                              Text(
-                                '거래 희망 장소',
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ],
+                          Text(
+                            '옹골찬',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
                           ),
-                          Text('강남구 역삼동', style: TextStyle(fontSize: 15),),
                         ],
                       ),
                       Container(
-                        margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                        height: 400,
-                        child: GoogleMap(
-                          initialCameraPosition: const CameraPosition(
-                            target: LatLng(37.5013068, 127.0396597),
-                            zoom: 17,
-                          ),
-                          zoomControlsEnabled: false,
-                          myLocationButtonEnabled: false,
-                          myLocationEnabled: true,
-                          markers: Set<Marker>.of(_marker),
-                          mapToolbarEnabled: false,
-                        ),
+                        height: 50,
+                        width: 50,
+                        color: Colors.yellow,
                       )
                     ],
-                  )),
-            ],
+                  ),
+                ),
+                Container(height: 1, color: Colors.black26),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    child: Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              '호주 달러 50달러 팔아요',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 10),
+                            Text('1시간 전'),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          '7월에 호주 여행 다녀와서 남은 돈이에요 역삼역 근처에서 직거래 희망합니다',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(height: 10),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on,
+                                  color: Colors.red,
+                                ),
+                                Text(
+                                  '거래 희망 장소',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              '강남구 역삼동',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          height: 400,
+                          child: GoogleMap(
+                            initialCameraPosition: const CameraPosition(
+                              target: LatLng(37.5013068, 127.0396597),
+                              zoom: 17,
+                            ),
+                            zoomControlsEnabled: false,
+                            myLocationButtonEnabled: false,
+                            myLocationEnabled: true,
+                            markers: Set<Marker>.of(_marker),
+                            mapToolbarEnabled: false,
+                          ),
+                        )
+                      ],
+                    )),
+                const SizedBox(height: 80),
+              ],
+            ),
           ),
-        ),
-        bottomNavigationBar: ,
+          Positioned(
+            bottom: 0,
+            child: Column(
+              children: [
+                Container(
+                    height: 1,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.black26),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    height: 80,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.favorite_border_rounded),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 10),
+                        Container(width: 1, color: Colors.black26),
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    children: [
+                                      SizedBox(width: 10),
+                                      CircleAvatar(
+                                        backgroundImage:
+                                        AssetImage('assets/images/australia.png'),
+                                        radius: 8,
+                                      ),
+                                      SizedBox(width: 5),
+                                      Text(
+                                        '50 AUD',
+                                        style: TextStyle(
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.blueAccent),
+                                      ),
+                                    ],
+                                  ),
+                                  Text('42,000원', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) {
+                                      return const ChattingDetailPage();
+                                    },
+                                  ));
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.fromLTRB(0, 5, 20, 5),
+                                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFFFD954),
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      '채팅하기',
+                                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
