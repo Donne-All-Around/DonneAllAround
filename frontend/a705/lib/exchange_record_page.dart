4d51f88f664a7d25b7911a43cac3e9e71fd10f01
yohwan <moonyh9600@gmail.com>
2023-09-20 16:20:58 +0900

    0920 YH 환전기록페이지 수정삭제 모달 추가

--- frontend/a705/lib/exchange_record_page.dart
+++ frontend/a705/lib/exchange_record_page.dart
@@ -8,6 +8,91 @@ class ExchangeRecordPage extends StatefulWidget {
   State<ExchangeRecordPage> createState() => ExchangeRecordPageState();
 }
 
+// 수정, 삭제 모달
+class CustomModalWidget extends StatelessWidget {
+  const CustomModalWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      content: Container(
+        width: 330,
+        height: 170,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    size: 28,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }
+                )
+              ]
+            ),
+            const SizedBox(height: 5),
+            InkWell(
+              onTap: () {
+                // 수정하기 동작 구현
+              },
+              child: Container(
+                width: 270,
+                height: 40,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: const Color(0xFF1D77E8)),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "수정하기",
+                    style: TextStyle(
+                      color: Color(0xFF1D77E8),
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                )
+              )
+            ),
+            const SizedBox(height: 15),
+            InkWell(
+              onTap: () {
+                // 삭제하기 동작 구현
+              },
+              child: Container(
+                width: 270,
+                height: 40,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: const Color(0xFFF53C3C)),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "삭제하기",
+                    style: TextStyle(
+                      color: Color(0xFFF53C3C),
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                )
+              )
+            )
+          ]
+        )
+      )
+    );
+  }
+}
+
+
+
 class ExchangeRecordPageState extends State<ExchangeRecordPage> {
   @override
   Widget build(BuildContext context) {
@@ -45,11 +130,57 @@ class ExchangeRecordPageState extends State<ExchangeRecordPage> {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: const Column(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      '환전 기록',
+                    const Text(
+                      '2022년 2월',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      )
+                    ),
+                    const SizedBox(height: 4),
+                    Container(
+                      width: 350,
+                      height: 125,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18.0),
+                        color: const Color(0xFFF2F2F2),
+                      ),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.all(16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '2022년 9월 20일 수요일',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.more_horiz,
+                                    color: Colors.black,
+                                  ),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return const CustomModalWidget();
+                                      }
+                                    );
+                                  }
+                                )
+                              ]
+                            )
+                          ),
+                        ]
+                      )
                     )
                   ]
                 )
