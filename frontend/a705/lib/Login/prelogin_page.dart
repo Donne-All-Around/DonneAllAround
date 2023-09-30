fce4071b5e409817a48b016d98b5c406700f74b6
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-09-30 11:39:57 +0900

    0930 HA 로그인 폼 작성

--- frontend/a705/lib/Login/prelogin_page.dart
+++ frontend/a705/lib/Login/prelogin_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../main_page.dart';
 
 
 class PreLoginPage extends StatefulWidget {
@@ -15,6 +17,10 @@ class _PreLoginPageState extends State<PreLoginPage> {
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _smsCodeController = TextEditingController();
   late String _verificationId;
+  bool _isButtonEnabled = false; // 인증문자 받기 버튼 활성화 상태를 저장 하는 변수
+  var _buttonText = '인증문자 받기';
+  bool _isStartEnabled = false; // 시작하기 버튼 활성화 상태 저장 하는 변수
+
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +77,13 @@ class _PreLoginPageState extends State<PreLoginPage> {
                       Column(
                         children: [
                           phoneNumberInput(),
+                          const SizedBox(height: 10,),
+                          submitButton(),
+                          const SizedBox(height: 20,),
+                          smsCodeInput(),
+                          const SizedBox(height: 10,),
+                          verifyButton(),
+
                         ],
                       ),
                     ],
@@ -142,4 +155,105 @@ class _PreLoginPageState extends State<PreLoginPage> {
       ),
     );
   }
+
+  ElevatedButton submitButton(){
+    return ElevatedButton(
+        onPressed: () async {
+          if (_key.currentState!.validate()) {
+            FirebaseAuth auth = FirebaseAuth.instance;
+            await auth.verifyPhoneNumber(
+              phoneNumber: _phoneController.text,
+                verificationCompleted: (PhoneAuthCredential credential) async {
+
+                await auth
+                    .signInWithCredential(credential)
+                    .then((value) => Navigator.push(context, MaterialPageRoute(builder: (context) => const MainPage())));
+                },
+                verificationFailed: (FirebaseAuthException e) {
+                if (e.code == 'invalid-phone-number') {
+                  print('유효하지 않은 번호임');
+                }
+                },
+                codeSent: (String verificationId, forceResendingToken) async {
+                String smsCode = _smsCodeController.text;
+                setState(() {
+                  _buttonText = '인증문자 재발송';
+                  _verificationId = verificationId;
+                });
+                },
+                codeAutoRetrievalTimeout: (verificationId){
+                print('시간초과');
+                });
+          }
+        },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: _isButtonEnabled ? Colors.white : Colors.grey[300],
+        elevation: 0,
+      ),
+      child: Container(
+      // margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+      height: 60,
+      width: double.infinity,
+      decoration:  BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.transparent,// 버튼 테두리 색상 변경
+        ),
+        color: _isButtonEnabled ? Colors.white : Colors.grey[300], // 버튼 색상 변경
+      ),
+      child: Center(
+        child: Text(
+          _buttonText,
+          style: TextStyle(
+            fontSize:25,
+            fontWeight: FontWeight.bold,
+            color: _isButtonEnabled ? Colors.black : Colors.grey, // 버튼 텍스트 색상 변경),
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+
+  ElevatedButton verifyButton() {
+    return ElevatedButton(
+        onPressed: () async {
+          FirebaseAuth auth = FirebaseAuth.instance;
+          
+          PhoneAuthCredential credential = PhoneAuthProvider.credential(
+              verificationId: _verificationId,
+
+              smsCode: _smsCodeController.text);
+          
+          await auth
+              .signInWithCredential(credential)
+              .then((value) => Navigator.push(context, MaterialPageRoute(builder: (context) => const MainPage())));
+
+        },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: _isButtonEnabled ? Colors.white : Colors.grey[300],
+        elevation: 0,
+      ),
+        child:Container(
+          // margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          height: 60,
+          width: double.infinity,
+          decoration:  BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            // border: Border.all(
+            // ),
+            color: _isStartEnabled ?  const Color(0xFFFFD954) : Colors.grey[300], // 버튼 색상 변경
+          ),
+          child:  Center(
+            child: Text(
+              '시작하기',
+              style: TextStyle(
+                fontSize:25,
+                fontWeight: FontWeight.bold,
+                color: _isStartEnabled ? Colors.black : Colors.grey,),
+            ),
+          ),
+        ),);
+  }
+
 }
