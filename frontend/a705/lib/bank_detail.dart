2c70f03bead4073ffd307d207d5d68ab83920c7d
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-03 21:14:00 +0900

    1003 HA 우선, 은행 상세 페이지 수수료 수정 중

--- frontend/a705/lib/bank_detail.dart
+++ frontend/a705/lib/bank_detail.dart
@@ -1,7 +1,10 @@
 import 'package:a705/chatting_detail_page.dart';
+import 'package:a705/providers/exchange_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+
+import 'models/BankDto.dart';
 
 class BankDetailPage extends StatefulWidget {
   final int selectedIndex;
@@ -43,11 +46,70 @@ class _BankDetailPageState extends State<BankDetailPage> {
 
   late int _idx;
   int _idx2 = 1;
-  
+
+
+
+ Map<String, double>? exchangeRates; // 환율 데이터를 저장할 변수
   @override
   void initState() {
     super.initState();
     _idx = widget.selectedIndex == 0 ? 0 : widget.selectedIndex + 1;
+    _fetchExchangeRates();
+  }
+
+  Future<void> _fetchExchangeRates() async {
+    try {
+      final exchangeProvider = ExchangeRateProvider();
+      final response = await exchangeProvider.fetchCurrencyData();
+      // API 응답 데이터 파싱
+      final exchangeResponse = response;
+      if (exchangeResponse.success) {
+        setState(() {
+          // 성공적으로 데이터를 받아왔을 때만 exchangeRates 업데이트
+          exchangeRates = {
+            'USDKRW': exchangeResponse.quotes.usdKrw,
+            'USDJPY': exchangeResponse.quotes.usdJpy,
+            'USDCNY': exchangeResponse.quotes.usdCny,
+            'USDEUR': exchangeResponse.quotes.usdEur,
+            'USDGBP': exchangeResponse.quotes.usdGbp,
+            'USDAUD': exchangeResponse.quotes.usdAud,
+            'USDCAD': exchangeResponse.quotes.usdCad,
+            'USDHKD': exchangeResponse.quotes.usdHkd,
+            'USDPHP': exchangeResponse.quotes.usdPhp,
+            'USDVND': exchangeResponse.quotes.usdVnd,
+            'USDTWD': exchangeResponse.quotes.usdTwd,
+            'USDSGD': exchangeResponse.quotes.usdSgd,
+            'USDCZK': exchangeResponse.quotes.usdCzk,
+            'USDNZD': exchangeResponse.quotes.usdNzd,
+            'USDRUB': exchangeResponse.quotes.usdRub,
+
+          };
+          _moneyController2.text = exchangeResponse.quotes.usdKrw.toStringAsFixed(2);
+        });
+      } else {
+        // API 요청은 성공했지만, 응답이 실패한 경우에 대한 처리
+        print('API 요청 성공, 응답 실패: ${exchangeResponse.terms}');
+      }
+    } catch (e) {
+      // API 요청 중 오류 발생
+      print('Error fetching exchange rates: $e');
+    }
+  }
+  double? calculateRate(String baseCurrency, String targetCurrency) {
+    if (exchangeRates != null &&
+        exchangeRates!.containsKey(baseCurrency) &&
+        exchangeRates!.containsKey(targetCurrency)) {
+      final baseRate = exchangeRates![baseCurrency];
+      final targetRate = exchangeRates![targetCurrency];
+
+      if (baseCurrency == 'USDKRW' && targetCurrency == 'USDJPY') {
+        return baseRate! / targetRate! ;
+      }  else {
+        return baseRate! / targetRate!;
+      }
+
+    }
+    return null;
   }
 
   List<String> currency1 = [
@@ -83,6 +145,11 @@ class _BankDetailPageState extends State<BankDetailPage> {
     '부산은행': {'currencyName': '부산은행', 'bankCode': '032'},
     'DGB대구은행': {'currencyName': 'DGB대구은행', 'bankCode': '031'},
   };
+
+
+
+
+
 
   String getToday() {
     DateTime now = DateTime.now();
@@ -634,7 +701,10 @@ class _BankDetailPageState extends State<BankDetailPage> {
                     ),
                     // 나라별 환율
                     const SizedBox(height: 20,),
-                    const ListViewBuilder(),
+                    ListViewBuilder(
+                      exchangeRates: exchangeRates,
+                      bankCode: widget.bankCode,
+                    ),
                   ],
                 ),
           ),
@@ -646,52 +716,93 @@ class _BankDetailPageState extends State<BankDetailPage> {
 }
 
 class ListViewBuilder extends StatefulWidget {
-  const ListViewBuilder({super.key});
+  final Map<String, double>? exchangeRates;
+  final String bankCode;
+  const ListViewBuilder({required this.exchangeRates, required this.bankCode,});
 
   @override
   State<ListViewBuilder> createState() => _ListViewBuilderState();
 }
 
 class _ListViewBuilderState extends State<ListViewBuilder> {
+  Map<String, double> exchangeRates = {};
+
+  double? calculateRate(String baseCurrency, String targetCurrency) {
+    if (exchangeRates.containsKey(baseCurrency) &&
+        exchangeRates.containsKey(targetCurrency)) {
+      final baseRate = exchangeRates[baseCurrency];
+      final targetRate = exchangeRates[targetCurrency];
+
+      if (baseRate != null && targetRate != null) {
+        if (baseCurrency == 'USDKRW' && targetCurrency == 'USDJPY') {
+          return baseRate / targetRate;
+        } else {
+          return baseRate / targetRate;
+        }
+      }
+    }
+    return null;
+  }
+
   final _valueList1 = [
     '미국(달러) USD',
     '일본(엔) JPY',
+    '중국(위안) CNY',
     '유럽(유로) EUR',
     '영국(파운드) GBP',
     '호주(달러) AUD',
-    '중국(위안) CNY',
-    '베트남(동) VND',
-    '한국(원) KRW',
-    '홍콩(달러) HKD',
     '캐나다(달러) CAD',
+    '홍콩(달러) HKD',
+    '필리핀(페소) PHP',
+    '베트남(동) VND',
+    '대만(달러) TWD',
+    '싱가폴(달러) SGD',
     '체코(코루나) CZK',
     '뉴질랜드(달러) NZD',
-    '필리핀(페소) PHP',
     '러시아(루블) RUB',
-    '싱가폴(달러) SGD',
-    '대만(달러) TWD',
   ];
   int idx1 = 0;
 
   List<String> currency1 = [
-    'USD',
-    'JPY',
-    'EUR',
-    'GBP',
-    'AUD',
-    'CNY',
-    'VND',
-    'KRW',
-    'HKD',
-    'CAD',
-    'CZK',
-    'NZD',
-    'PHP',
-    'RUB',
-    'SGD',
-    'TWD',
+    'USDKRW',
+    'USDJPY',
+    'USDCNY',
+    'USDEUR',
+    'USDGBP',
+    'USDAUD',
+    'USDCAD',
+    'USDHKD',
+    'USDPHP',
+    'USDVND',
+    'USDTWD',
+    'USDSGD',
+    'USDCZK',
+    'USDNZD',
+    'USDRUB',
   ];
 
+  Map<String, Map<String, String>> bankInfo = {
+    '하나은행': {'currencyName': '하나은행', 'bankCode': '081'},
+    '우리은행': {'currencyName': '우리은행', 'bankCode': '020'},
+    'KB국민은행': {'currencyName': 'KB국민은행', 'bankCode': '004'},
+    '신한은행': {'currencyName': '신한은행', 'bankCode': '088'},
+    'NH농협은행': {'currencyName': 'NH농협은행', 'bankCode': '011'},
+    'IBK기업은행': {'currencyName': 'IBK기업은행', 'bankCode': '003'},
+    'SC제일은행': {'currencyName': 'SC제일은행', 'bankCode': '023'},
+    '시티은행': {'currencyName': '시티은행', 'bankCode': '027'},
+    'Sh수협은행': {'currencyName': 'Sh수협은행', 'bankCode': '007'},
+    '부산은행': {'currencyName': '부산은행', 'bankCode': '032'},
+    'DGB대구은행': {'currencyName': 'DGB대구은행', 'bankCode': '031'},
+  };
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    // widget.exchangeRates에서 데이터를 가져와서 사용할 변수에 할당
+    exchangeRates = widget.exchangeRates ?? {};
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -701,6 +812,50 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
       shrinkWrap: true,
       itemCount: _valueList1.length,
       itemBuilder: (context, index) {
+        final selectedBankCode = widget.bankCode;
+//
+//         // 선택된 은행 코드와 일치하는 은행 정보 가져오기
+//         final bankInfoData = bankInfo[selectedBankCode];
+//         if (bankInfoData != null) {
+//           final fees = bankInfoData['fees'];
+//           if (fees != null) {
+//             final feeInfo = fees[currency1[index]!];
+//             if (feeInfo != null) {
+//               feeDescription = '${feeInfo}%';
+//             }
+//           }
+//         }
+//         // 현찰 살 때 수수료
+//         final buyingFee = bankInfoData['fees'][currency1[index]]['buyingFee'];
+// // 현찰 팔 때 수수료
+//         final sellingFee = bankInfoData['fees'][currency1[index]]['sellingFee'];
+// // 송금 보낼 때 수수료
+//         final sendingFee = bankInfoData['fees'][currency1[index]]['sendingFee'];
+
+        String? formattedRate = 'N/A'; // 초기값 설정
+
+        if (currency1[index] == 'USDKRW') {
+          // 미국 달러(USD)은 그대로 표시
+          final exchangeRate = exchangeRates[currency1[index]];
+          if (exchangeRate != null) {
+            formattedRate = exchangeRate.toStringAsFixed(2);
+          }
+        } else if(currency1[index]== 'USDJPY' ) {
+          var rate = calculateRate('USDKRW', currency1[index]);
+          if (rate != null) {
+            rate *= 100;
+          }
+          formattedRate = rate?.toStringAsFixed(2);
+                }
+        else {
+          // 다른 국가의 환율 계산
+          final rate = calculateRate('USDKRW', currency1[index]);
+          if (rate != null) {
+            formattedRate = rate.toStringAsFixed(2);
+          }
+        }
+
+
         return Row(
           children: [
             Expanded(
@@ -729,8 +884,8 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
                         Row(
                           children: [
                             CircleAvatar(
-                              backgroundImage: AssetImage(
-                                  'assets/images/flag/${currency1[index]}.png'),
+                              backgroundImage:
+                              AssetImage('assets/images/flag/${currency1[index]}.png'),
                               radius: 10,
                             ),
                             const SizedBox(width: 5),
@@ -740,23 +895,23 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
                             ),
                           ],
                         ),
-                        const Row(
+                         Row(
                           children: [
                             Text(
-                              '1,300.00 원',
+                              '$formattedRate 원',
                               textAlign: TextAlign.end,
-                              style: TextStyle(fontSize: 17, color: Colors.red),
+                              style: const TextStyle(fontSize: 17, color: Colors.red),
                             ),
                           ],
                         )
                       ],
                     ),
                     const SizedBox(height: 10,),
-                    const Row(
+                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Column(
+                        const Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             SizedBox(height: 20),
@@ -780,7 +935,7 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
 
                         Row(
                           children: [
-                            Column(
+                            const Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Text(
@@ -816,41 +971,41 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
                               ],
                             ),
                             SizedBox(width: 20),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  '수수료',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    Text(
-                                      '1.75%',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                          height: 1.532),
-                                    ),
-                                    Text(
-                                      '1.75%',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                          height: 1.532),
-                                    ),
-                                    Text(
-                                      '1.75%',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                          height: 1.532),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
+                            // Column(
+                            //   crossAxisAlignment: CrossAxisAlignment.center,
+                            //   children: [
+                            //     Text(
+                            //       '수수료',
+                            //       style: TextStyle(fontSize: 15),
+                            //     ),
+                            //     Column(
+                            //       crossAxisAlignment: CrossAxisAlignment.end,
+                            //       children: [
+                            //         Text(
+                            //           '${fees?.buying ?? "서비스 미제공"}%',
+                            //           style: TextStyle(
+                            //               fontWeight: FontWeight.bold,
+                            //               fontSize: 16,
+                            //               height: 1.532),
+                            //         ),
+                            //         Text(
+                            //           '${fees?.selling ?? "서비스 미제공"}%',
+                            //           style: TextStyle(
+                            //               fontWeight: FontWeight.bold,
+                            //               fontSize: 16,
+                            //               height: 1.532),
+                            //         ),
+                            //         Text(
+                            //           '${fees?.sending ?? "서비스 미제공"}% ',
+                            //           style: TextStyle(
+                            //               fontWeight: FontWeight.bold,
+                            //               fontSize: 16,
+                            //               height: 1.532),
+                            //         )
+                            //       ],
+                            //     ),
+                            //   ],
+                            // ),
                           ],
                         ),
                       ],
